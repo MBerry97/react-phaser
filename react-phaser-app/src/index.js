2b9eb5863d45980fd2039d737f2fc6630b4de293
CharlyMannion <charlottemannion100@gmail.com>
2020-11-03 11:19:36 +0000

    box goes down when pointer click

--- react-phaser-app/src/index.js
+++ react-phaser-app/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 
 
 ReactDOM.render(
-  
-    <App />
-  ,
-  document.getElementById('root')
-);
 
+    <
+    App / > ,
+    document.getElementById('root')
+);
